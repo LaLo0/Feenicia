5d468bcbad479511f4dc71e1d9300bc0a2e99ae1
LaLo0 <lalo.deams@icloud.com>
2017-05-30 17:38:05 -0500

    user y pass

--- app/src/main/java/com/serti/miExamen/MainActivity.java
+++ app/src/main/java/com/serti/miExamen/MainActivity.java
@@ -3,6 +3,7 @@ package com.serti.miExamen;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final EditText user = (EditText) findViewById(R.id.user);
-        final EditText pass =(EditText) findViewById(R.id.pass);
+        final EditText pass = (EditText) findViewById(R.id.pass);
         Button Bingresa = (Button) findViewById(R.id.Bingresa);
+
 
         Bingresa.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,15 +29,27 @@ public class MainActivity extends AppCompatActivity {
                     Toast datos = Toast.makeText(getApplicationContext(), "Faltan datos", Toast.LENGTH_LONG);
                     datos.show();
 
-                }else{
+                } else {
                     //Toast datos1 = Toast.makeText(getApplicationContext(), "Datos OK", Toast.LENGTH_LONG);
                     //datos1.show();
+                    //Intent intent = new Intent(v.getContext(), MenuUsuario.class);
+                    //startActivityForResult(intent, 0);
 
-                    Intent intent = new Intent(v.getContext(), MenuUsuario.class);
-                    startActivityForResult(intent, 0);
+                    //if(user.getText().toString().isEmpty() && pass.getText().toString() == "admin"){
+
+                    if (user.equals("lalo") && pass.equals("admin")) {
+                        Log.d("TAG", "Has iniciado sesion con exito " + user.getText().toString());
+                        Toast datos1 = Toast.makeText(getApplicationContext(), "Bienvenido: " + user.getText().toString(), Toast.LENGTH_LONG);
+                        datos1.show();
+
+                        Intent intent = new Intent(v.getContext(), MenuUsuario.class);
+                        startActivityForResult(intent, 0);
+                    } else {
+                        Toast datos2 = Toast.makeText(getApplicationContext(), "Usuario incorrecto, estas pendeje", Toast.LENGTH_LONG);
+                        datos2.show();
+                    }
                 }
             }
         });
-
     }
 }
