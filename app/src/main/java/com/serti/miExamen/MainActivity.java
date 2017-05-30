848fc21868a07f754a9a1d2d8e2d3bb2dfeba330
LaLo0 <lalo.deams@icloud.com>
2017-05-30 15:01:11 -0500

    Activities y validacion de campos vacios

--- app/src/main/java/com/serti/miExamen/MainActivity.java
+++ app/src/main/java/com/serti/miExamen/MainActivity.java
@@ -1,7 +1,12 @@
 package com.serti.miExamen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText user = (EditText) findViewById(R.id.user);
+        final EditText pass =(EditText) findViewById(R.id.pass);
+        Button Bingresa = (Button) findViewById(R.id.Bingresa);
+
+        Bingresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.length() == 0 || pass.length() == 0) {
+
+                    Toast datos = Toast.makeText(getApplicationContext(), "Faltan datos", Toast.LENGTH_LONG);
+                    datos.show();
+
+                }else{
+                    //Toast datos1 = Toast.makeText(getApplicationContext(), "Datos OK", Toast.LENGTH_LONG);
+                    //datos1.show();
+
+                    Intent intent = new Intent(v.getContext(), MenuUsuario.class);
+                    startActivityForResult(intent, 0);
+                }
+            }
+        });
+
     }
 }
